c03ef9b1d7c1187257aa08c626edd2ca919cf746
robertobuso <robertobuso@gmail.com>
2018-09-29 14:35:30 -0400

    applied css styling created popup functionality fixed searchbar to top new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ User.create(name:"Kevin",username:"kevin01", img_url:"https://vignette.wikia.noc
 User.create(name:"Tony",username:"tony01", img_url:"https://www.telegraph.co.uk/content/dam/news/2017/11/30/TELEMMGLPICT000148018716_trans_NvBQzQNjv4BqutIaqbtdgycbjoKap7Ft85iru5ESH6waxLG5-q_DX4Y.jpeg?imwidth=450", hobby:"chess")
 
 User.create(name:"Eric",username:"eric01", img_url:"https://pmcvariety.files.wordpress.com/2018/04/eric-bana.jpg?w=1000", hobby:"balloons")
+
+User.create(name:"Scarlett",username:"scarlett01", img_url:"http://www.electricsheepmagazine.co.uk/features/wp-content/uploads/2011/12/review_AlterEgo_Scarlett.jpg", hobby:"games")
+
+User.create(name:"Bryan",username:"bryan01", img_url:"https://uproxx.files.wordpress.com/2016/03/walterwhite-uproxx.jpg?quality=95&w=650", hobby:"games")
+
+User.create(name:"Rosa",username:"rosa01", img_url:"http://www.hotel-r.net/im/hotel/es/la-rosa-20.jpg", hobby:"music")
+
+User.create(name:"Tyrell",username:"tyrell01", img_url:"https://imgix.ranker.com/user_node_img/50025/1000496042/original/margaery-tyrell-tv-characters-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces", hobby:"unity")
