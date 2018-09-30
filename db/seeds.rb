b08f0eb42aac2e3ab335df1e09e3d24a63aae8c9
robertobuso <robertobuso@gmail.com>
2018-09-30 19:56:59 -0400

    added languages and mods to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name:"Kevin",username:"kevin01", img_url:"https://vignette.wikia.nocookie.net/p__/images/5/5d/Home_alone_macaulay_culkin_kevin_mccallister_boy_fear_shout_fright_346_1600x1200.jpg/revision/latest?cb=20140320134854&path-prefix=protagonist", hobby:"darts")
+User.create(name:"Kevin",username:"kevin01", img_url:"https://vignette.wikia.nocookie.net/p__/images/5/5d/Home_alone_macaulay_culkin_kevin_mccallister_boy_fear_shout_fright_346_1600x1200.jpg/revision/latest?cb=20140320134854&path-prefix=protagonist", hobby:"darts", language: "ruby", mod: "mod_4")
 
-User.create(name:"Tony",username:"tony01", img_url:"https://www.telegraph.co.uk/content/dam/news/2017/11/30/TELEMMGLPICT000148018716_trans_NvBQzQNjv4BqutIaqbtdgycbjoKap7Ft85iru5ESH6waxLG5-q_DX4Y.jpeg?imwidth=450", hobby:"chess")
+User.create(name:"Tony",username:"tony01", img_url:"https://www.telegraph.co.uk/content/dam/news/2017/11/30/TELEMMGLPICT000148018716_trans_NvBQzQNjv4BqutIaqbtdgycbjoKap7Ft85iru5ESH6waxLG5-q_DX4Y.jpeg?imwidth=450", hobby:"chess", language: "javascript", mod: "staff")
 
-User.create(name:"Eric",username:"eric01", img_url:"https://pmcvariety.files.wordpress.com/2018/04/eric-bana.jpg?w=1000", hobby:"balloons")
+User.create(name:"Eric",username:"eric01", img_url:"https://pmcvariety.files.wordpress.com/2018/04/eric-bana.jpg?w=1000", hobby:"balloons", language: "python", mod: "staff")
 
-User.create(name:"Scarlett",username:"scarlett01", img_url:"http://www.electricsheepmagazine.co.uk/features/wp-content/uploads/2011/12/review_AlterEgo_Scarlett.jpg", hobby:"games")
+User.create(name:"Scarlett",username:"scarlett01", img_url:"http://www.electricsheepmagazine.co.uk/features/wp-content/uploads/2011/12/review_AlterEgo_Scarlett.jpg", hobby:"games", language: "ruby", mod: "mod_5")
 
-User.create(name:"Bryan",username:"bryan01", img_url:"https://uproxx.files.wordpress.com/2016/03/walterwhite-uproxx.jpg?quality=95&w=650", hobby:"games")
+User.create(name:"Bryan",username:"bryan01", img_url:"https://uproxx.files.wordpress.com/2016/03/walterwhite-uproxx.jpg?quality=95&w=650", hobby:"games", language: "assembly", mod: "mod_2")
 
-User.create(name:"Rosa",username:"rosa01", img_url:"http://www.hotel-r.net/im/hotel/es/la-rosa-20.jpg", hobby:"music")
+User.create(name:"Rosa",username:"rosa01", img_url:"http://www.hotel-r.net/im/hotel/es/la-rosa-20.jpg", hobby:"music", language: "spanish", mod: "mod_4")
 
-User.create(name:"Tyrell",username:"tyrell01", img_url:"https://imgix.ranker.com/user_node_img/50025/1000496042/original/margaery-tyrell-tv-characters-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces", hobby:"unity")
+User.create(name:"Tyrell",username:"tyrell01", img_url:"https://imgix.ranker.com/user_node_img/50025/1000496042/original/margaery-tyrell-tv-characters-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces", hobby:"unity", language: "logo", mod: "mod_5")
