d1df612c8d0a34b886ef2b613e822857d5c8ea7d
robertobuso <robertobuso@gmail.com>
2018-10-02 09:50:50 -0400

    started work on password authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_190951) do
+ActiveRecord::Schema.define(version: 2018_10_02_123532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_09_28_190951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url"
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
