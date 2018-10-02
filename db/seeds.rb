97c85573f9b4c538a70990789b261726931bd8b3
Kevin RRivera <krivera1015@gmail.com>
2018-10-02 09:51:42 -0400

    Fixed minor bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kevin = User.create(username: "kev", name: "kevin", mod: 4, language: "javascript", hobby: "dancing")
+roberto = User.create(username: "rob", name: "roberto", mod: 4, language: "ruby", hobby: "acting")
+amirata = User.create(username: "ami", name: "amirata", mod: 4, language: "javascript", hobby: "git pushing")
+prince = User.create(username: "king", name: "prince", mod: 4, language: "rails", hobby: "dancing")
+scarlet = User.create(username: "scar", name: "scarlet", mod: 4, language: "react", hobby: "cambio")
+gaby = User.create(username: "baby", name: "gaby", mod: 4, language: "ruby", hobby: "coding")
