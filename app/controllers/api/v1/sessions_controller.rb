699010bc0318a3e48ffbdfcb959bef931d4a62cf
robertobuso <robertobuso@gmail.com>
2018-10-02 14:38:18 -0400

    before merge tuesdaypm

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,27 +4,18 @@ class Api::V1::SessionsController < ApplicationController
     end
 
     def create
-
         @users = User.all
         @user = @users.find{ |inst| inst.username == params[:username] }
 
         if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
             render json: @users
         else
-          render 
+          render json: { errors: "Don't you know your username and password???" }, status: :unprocessable_entity
         end
     end
 
     def delete
         session.delete(:user_id)
     end
-
-    private
-
-    def session_params
-      params.permit("_json", "username", "password")
-    end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create, :update, :delete]
 
       get '/login', to: 'sessions#new', as: 'login'
       post '/login', to: 'sessions#create'
 
-delete '/logout', to: 'sessions#delete', as: 'logout'
+      delete '/logout', to: 'sessions#delete', as: 'logout'
+
+      resources :users, only: [:index, :create, :update, :delete]
+
+
     end
   end
 end
