3dcf68d2afa820b95493cc506fa3ecbddc4f7ffe
Kevin RRivera <krivera1015@gmail.com>
2018-10-02 17:08:12 -0400

    Fixed Users Params and Added User to Users.all

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,9 @@ class Api::V1::UsersController < ApplicationController
       if @users.find { |inst| inst.username == @user.username}
         render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
       elsif @user.save
-        render json: @user, status: :accepted
+        @users = User.all
+        # byebug
+        render json: @users, status: :accepted
       else
         render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
       end
@@ -34,7 +36,7 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-      params.permit(:user, :username, :name, :mod, :language, :eye_color, :height, :hobby, :description, :password)
+      params.permit(:user, :username, :name, :mod, :language, :eye_color, :height, :hobby, :img_url, :description, :password)
     end
 
     def find_user
